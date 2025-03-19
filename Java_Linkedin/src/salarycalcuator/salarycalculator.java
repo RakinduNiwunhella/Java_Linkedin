9999ffb8869130e917d57e9316de2e8da4b6cb0f
Rakindu Niwunhella <rakindu15@gmail.com>
2025-03-19 09:19:25 +0530

    up

---
+++ Java_Linkedin/src/salarycalcuator/salarycalculator.java
@@ -0,0 +1,23 @@
+package salarycalcuator;
+import org.w3c.dom.ls.LSOutput;
+
+import java.util.Scanner;
+
+public class salarycalculator {
+    public static void main(String[] args) {
+        int salary = 1000;
+        int bonus = 250;
+        int quota = 10;
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("How many sales did the employee make this week? ");
+        int sales = scanner.nextInt();
+        scanner.close();
+
+
+
+
+
+    }
+
+}
