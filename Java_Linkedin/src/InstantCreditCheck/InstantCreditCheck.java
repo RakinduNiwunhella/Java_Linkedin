57911283809daae942dac47193391ae60203c595
Rakindu Niwunhella <rakindu15@gmail.com>
2025-03-22 13:09:02 +0530

    up

--- Java_Linkedin/src/InstantCreditCheck/InstantCreditCheck.java
+++ Java_Linkedin/src/InstantCreditCheck/InstantCreditCheck.java
@@ -1,4 +1,5 @@
 package InstantCreditCheck;
+import java.security.spec.RSAOtherPrimeInfo;
 import java.util.Scanner;
 public class InstantCreditCheck {
     public static void main(String[] args) {
@@ -8,11 +9,19 @@ public class InstantCreditCheck {
         double salary = scanner.nextDouble();
 
         System.out.println("Enter your credit score: ");
-        double score = scanner.nextDouble();
-
-        //
-
+        int score = scanner.nextInt();
+        //check if the user is qualified
+        boolean qualified = isUserQualified(score,salary);
     }
-
+public static boolean isUserQualified(int creditScore, double salary){
+        double requiredSalary = 25000;
+        int requiredCreditScore = 700;
+        if (creditScore >= requiredCreditScore && salary >= requiredSalary){
+           return true;
+        }else {
+            System.out.println("Sorry, You've bee declined.");
+            return false;
+        }
+    }
 
 }
