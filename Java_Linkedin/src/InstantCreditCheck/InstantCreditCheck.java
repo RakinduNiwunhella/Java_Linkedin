a99daeafcfacae053c756fc97e685b661723eafa
Rakindu Niwunhella <rakindu15@gmail.com>
2025-03-22 12:59:07 +0530

    up

---
+++ Java_Linkedin/src/InstantCreditCheck/InstantCreditCheck.java
@@ -0,0 +1,18 @@
+package InstantCreditCheck;
+import java.util.Scanner;
+public class InstantCreditCheck {
+    public static void main(String[] args) {
+    //get input
+        Scanner scanner = new Scanner (System.in);
+        System.out.println("Enter your salary: ");
+        double salary = scanner.nextDouble();
+
+        System.out.println("Enter your credit score: ");
+        double score = scanner.nextDouble();
+
+        //
+
+    }
+
+
+}
